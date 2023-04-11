969ea974501e775884dc34dbf54fcb192855a382
Jonathan Miller <jon@jmill.dev>
2023-04-11 15:02:11 -0400

    Watcher works

--- eden-subgraph/src/mappings/mapping.ts
+++ eden-subgraph/src/mappings/mapping.ts
@@ -4,7 +4,7 @@ import { MintEvent, LivemintToken } from "../../generated/schema";
 export function handleMint(event: Mint): void {
   let mintEvent = new MintEvent(event.transaction.hash);
   if (mintEvent) {
-    mintEvent.timestamp = event.block.timestamp;
+    mintEvent.timestamp = event.block.number;
     mintEvent.txHash = event.transaction.hash;
     mintEvent.caller = event.transaction.from;
     mintEvent.tokenId = event.params._tokenId;
